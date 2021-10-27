8921752cc995e9c6acfc3680bb336c00f5a8b7e1
--local <jaeckanaquth@gmail.com>
2021-10-27 22:01:28 +0530

    jbljblk

--- github_config.py
+++ github_config.py
@@ -1,15 +1,15 @@
 import os
 
-password = str(os.getenv('WP_PASSWORD'))
-user = str(os.getenv('WP_USER'))
-url = str(os.getenv('URL'))
-novel_name = str(os.getenv('NOVEL_NAME'))
-my_site = str(os.getenv('MY_SITE'))
-translation_site = str(os.getenv('TRANSLATION_SITE'))
-nu_username = str(os.getenv('NU_USER'))
-nu_password = str(os.getenv('NU_PASSWORD'))
-translator = str(os.getenv('TRANSLATOR'))
-special = list(os.getenv('SPECIAL'))
-tags = list(os.getenv('TAGS'))
-novel_link = str(os.getenv('NOVEL_LINK')) + \
+password = str(os.environ.get('WP_PASSWORD'))
+user = str(os.environ.get('WP_USER'))
+url = str(os.environ.get('URL'))
+novel_name = str(os.environ.get('NOVEL_NAME'))
+my_site = str(os.environ.get('MY_SITE'))
+translation_site = str(os.environ.get('TRANSLATION_SITE'))
+nu_username = str(os.environ.get('NU_USER'))
+nu_password = str(os.environ.get('NU_PASSWORD'))
+translator = str(os.environ.get('TRANSLATOR'))
+special = list(os.environ.get('SPECIAL'))
+tags = list(os.environ.get('TAGS'))
+novel_link = str(os.environ.get('NOVEL_LINK')) + \
     novel_name.lower().replace(" ", '-') + '/'
