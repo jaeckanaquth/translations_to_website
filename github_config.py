ca830c2263ee56a78634045eb8bc789461ebf167
--local <jaeckanaquth@gmail.com>
2021-10-27 22:52:42 +0530

    env changes

--- github_config.py
+++ github_config.py
@@ -1,15 +1,15 @@
 import os
 
-password = str(os.environ.get('WP_PASSWORD'))
-user = str(os.environ.get('WP_USER'))
-url = str(os.environ.get('URL'))
+password = str(os.environ['WP_PASSWORD'])
+user = str(os.environ['WP_USER'])
+url = str(os.environ['URL'])
 print(password, user, url)
-novel_name = str(os.environ.get('NOVEL_NAME'))
-my_site = str(os.environ.get('MY_SITE'))
-translation_site = str(os.environ.get('TRANSLATION_SITE'))
-nu_username = str(os.environ.get('NU_USER'))
-nu_password = str(os.environ.get('NU_PASSWORD'))
-translator = str(os.environ.get('TRANSLATOR'))
+novel_name = str(os.environ['NOVEL_NAME'])
+my_site = str(os.environ['MY_SITE'])
+translation_site = str(os.environ['TRANSLATION_SITE'])
+nu_username = str(os.environ['NU_USER'])
+nu_password = str(os.environ['NU_PASSWORD'])
+translator = str(os.environ['TRANSLATOR'])
 special = ['[', ']', '\\', '\'', '!', '@', '#', '$', '%', '^', '&', '*','{', '}', '"', ':', ';', ',', '<', '>', '.', '?', '<WordPressPost: b\'', 'WordPressPost: ', '\'>']
-tags = [os.environ.get('TAGS')]
-novel_link = str(os.environ.get('NOVEL_LINK')) + novel_name.lower().replace(" ", '-') + '/'
+tags = [os.environ['TAGS']]
+novel_link = str(os.environ['NOVEL_LINK']) + novel_name.lower().replace(" ", '-') + '/'
