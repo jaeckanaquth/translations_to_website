8cbaa565c86d88f7c597567000febe86289509f0
--local <jaeckanaquth@gmail.com>
2021-10-28 22:06:46 +0530

    Add user_agent

--- page_translator.py
+++ page_translator.py
@@ -37,7 +37,12 @@ def page_translate(url):
     return text
 
 def header_name(url):
-    user_agent = {'User-agent': 'Mozilla/5.0'}
+    user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+                  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                  'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+                  'Accept-Encoding': 'none',
+                  'Accept-Language': 'en-US,en;q=0.8',
+                  'Connection': 'keep-alive'}
     novel = requests.get(url, headers=user_agent)
     novel.raise_for_status()
     novel.encoding = "GBK"
