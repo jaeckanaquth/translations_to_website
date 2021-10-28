7153e16850d5406315cdd5ef0f0159ac48172af7
--local <jaeckanaquth@gmail.com>
2021-10-28 14:35:02 +0530

    novel_name.lower().replace(" ", '-')

--- github_config.py
+++ github_config.py
@@ -12,4 +12,5 @@ nu_password = str(os.environ['NU_PASSWORD'])
 translator = str(os.environ['TRANSLATOR'])
 special = ['[', ']', '\\', '\'', '!', '@', '#', '$', '%', '^', '&', '*','{', '}', '"', ':', ';', ',', '<', '>', '.', '?', '<WordPressPost: b\'', 'WordPressPost: ', '\'>']
 tags = [os.environ['TAGS']]
-novel_link = f"{str(os.environ['NOVEL_LINK'])}{novel_name.lower().replace(" ", '-')}/"
+name_edit = novel_name.lower().replace(" ", '-')
+novel_link = f"{os.environ['NOVEL_LINK']}{name_edit}/"
