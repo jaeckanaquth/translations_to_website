fc1e0d1975644af1017054e95d32863727e77097
--local <jaeckanaquth@gmail.com>
2021-10-29 00:39:03 +0530

    git config

--- github_config.py
+++ github_config.py
@@ -2,14 +2,14 @@ import os
 
 password = str(os.environ['WP_PASSWORD'])
 user = str(os.environ['WP_USER'])
-url = f"{os.environ['URL']}"
-novel_name = str(os.environ['NOVEL_NAME'])
+url = f'https://www.mbtxt.la/go/91929/'
+novel_name = "I Became a Heartthrob After A Job in Quick Transmigration"
 my_site = f"{os.environ['MY_SITE']}"
-translation_site = str(os.environ['TRANSLATION_SITE'])
+translation_site = f'https://www.mbtxt.la/go/'
 nu_username = str(os.environ['NU_USER'])
 nu_password = str(os.environ['NU_PASSWORD'])
 translator = str(os.environ['TRANSLATOR'])
 special = ['[', ']', '\\', '\'', '!', '@', '#', '$', '%', '^', '&', '*','{', '}', '"', ':', ';', ',', '<', '>', '.', '?', '<WordPressPost: b\'', 'WordPressPost: ', '\'>']
-tags = [os.environ['TAGS']]
+tags = ['IBHAJQT']
 name_edit = novel_name.lower().replace(" ", '-')
 novel_link = f"{os.environ['NOVEL_LINK']}{name_edit}/"
