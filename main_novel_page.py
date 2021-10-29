0831baf167c9161d74d2bd13cf8d9a6e2198d0a8
--local <jaeckanaquth@gmail.com>
2021-10-29 13:13:04 +0530

    Add image

--- main_novel_page.py
+++ main_novel_page.py
@@ -11,28 +11,28 @@ from requests.auth import HTTPBasicAuth
 
 user_agent = {'User-Agent': 'Mozilla/5.0'}
 
-# def main_img():
-#     session = requests.Session()
-#     novel = session.get(url, headers=user_agent)
-#     novel.raise_for_status()
-#     print(novel.status_code)
-#     novel.encoding = "GBK"
-#     novelSoup = bs(novel.text, "html.parser")
-#     novel_img = novelSoup.findAll('img')
-#     novel_img = novel_img[0]['src']
-#     img_data = requests.get(novel_img).content
-#     return novel_img, img_data
+def main_img():
+    session = requests.Session()
+    novel = session.get(url, headers=user_agent)
+    novel.raise_for_status()
+    print(novel.status_code)
+    novel.encoding = "GBK"
+    novelSoup = bs(novel.text, "html.parser")
+    novel_img = novelSoup.find("img", {"class": "thumbnail"})
+    novel_img = novel_img['src']
+    img_data = requests.get(novel_img).content
+    return novel_img, img_data
 
 filename = novel_name.replace(" ", "-") + ".jpg"
 published = pd.DataFrame(columns=["name", "post_id", "link_id", "wp_link"])
 
-# if filename not in published["name"].to_list():
-#     novel_img, img_data = main_img()
-#     img_id = uploadImage(novel_img, img_data)
-#     # img_id = 'abc'
-#     dct = {"name": filename, "post_id": img_id, "link_id": novel_img, "wp_link": novel_link + img_id}
-#     published = published.append(dct, ignore_index=True)
-#     print(published)
+if filename not in published["name"].to_list():
+    novel_img, img_data = main_img()
+    img_id = uploadImage(novel_img, img_data)
+    # img_id = 'abc'
+    dct = {"name": filename, "post_id": img_id, "link_id": novel_img, "wp_link": novel_link + img_id}
+    published = published.append(dct, ignore_index=True)
+    print(published)
         
 #save novel chapters
 page_publishandlink(published)
