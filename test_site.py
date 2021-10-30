e7635b509eb2aa8e2be8077f969e8e3480da4305
anmol-dudani-drishya <anmol.dudani@drishya.ai>
2021-10-31 00:20:31 +0530

    testting 123

--- test_site.py
+++ test_site.py
@@ -10,7 +10,7 @@ import urllib.request
 url = 'https://www.shubaow.net/127_127391/'
 
 # Open the URL as Browser, not as python urllib
-page = urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"})
+page = urllib.request.Request(url)
 infile = urllib.request.urlopen(page).read()
 # Read the content as string decoded with ISO-8859-1
 data = infile.decode('ISO-8859-1')
