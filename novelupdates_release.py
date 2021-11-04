c6d6dabbc8089c9cb0750b42aaab4066de69add2
anmol-dudani-drishya <anmol.dudani@drishya.ai>
2021-11-04 18:32:03 +0530

    extra code

--- novelupdates_release.py
+++ novelupdates_release.py
@@ -2,16 +2,17 @@ from selenium import webdriver
 import glob
 from time import sleep
 if glob.glob("config.py"):
-    from config import nu_username, nu_password, novel_name, translator
+    from config import nu_username, nu_password, translator
+    import nu_config
 else:
     from github_config import nu_username, nu_password, novel_name, translator
-
 
 def page_linktonu(release_link_url, header):
     url = "https://www.novelupdates.com/add-release/"
     login = "https://www.novelupdates.com/login/"
 
-    driver = webdriver.Firefox()
+    driver = webdriver.Edge(
+        executable_path="C:/Users/jaeck/Documents/personal_workspace/downloads/msedgedriver.exe")
     driver.get(login)
     # sleep(30)
     username = driver.find_element_by_id("user_login")
@@ -23,31 +24,31 @@ def page_linktonu(release_link_url, header):
 
     driver.get(url)
     sleep(30)
-    try:
-        group = driver.find_element_by_id("group_change_100").clear()
-        group = driver.find_element_by_id("group_change_100")
-        group.send_keys(translator)
-        sleep(30)
-        group = driver.find_element_by_id("group_change_100")
-        driver.find_element_by_class_name("change_list").click()
 
-        release_link = driver.find_element_by_id("arlink")
-        release_link.send_keys(release_link_url)
+    group = driver.find_element_by_id("group_change_100").clear()
+    group = driver.find_element_by_id("group_change_100")
+    group.send_keys(nu_config.translator)
+    sleep(15)
+    group = driver.find_element_by_id("group_change_100")
+    driver.find_element_by_class_name("change_list").click()
 
-        title = driver.find_element_by_id("title_change_100").clear()
-        title = driver.find_element_by_id("title_change_100")
-        title.send_keys(novel_name)
-        sleep(30)
-        title = driver.find_element_by_id("title_change_100")
-        driver.find_element_by_class_name("change_list").click()
+    release_link = driver.find_element_by_id("arlink")
+    release_link.send_keys(release_link_url)
 
-        release_chapter = driver.find_element_by_id("arrelease")
-        release_chapter.send_keys(header)
+    title = driver.find_element_by_id("title_change_100").clear()
+    title = driver.find_element_by_id("title_change_100")
+    title.send_keys(nu_config.novel_name)
+    sleep(30)
+    title = driver.find_element_by_id("title_change_100")
+    driver.find_element_by_class_name("change_list").click()
 
-    except Exception as e:
-        print("Error of NU:" + str(e))
-        exit()
+    release_chapter = driver.find_element_by_id("arrelease")
+    release_chapter.send_keys(header)
 
     driver.find_element_by_name("submit").click()
     driver.close()
     return 'done'
+
+
+if glob.glob("nu_config.py"):
+    page_linktonu(nu_config.release_link_url, nu_config.header)
